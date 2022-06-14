0ed631acaf5d7b5e39001538305daefc22a06a11
iRaziul <raziul.cse@gmail.com>
2022-06-14 16:58:06 +0600

    First Commit

---
+++ src/ShurjoPayException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Raziul\ShurjoPay;
+
+/**
+ * ShurjoPayException
+ * 
+ * @author Raziul Islam <raziul.cse@gmail.com>
+ * @package Raziul\ShurjoPay
+ */
+class ShurjoPayException extends \Exception
+{
+}
